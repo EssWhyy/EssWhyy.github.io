b0465e125976f9fbd6e73231127a4965abf25ebe
Sven Tang <39799639+EssWhyy@users.noreply.github.com>
2021-02-20 23:48:26 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Fact Generator
-var factlist = ["I was Time's Person of The Year for 2006! :)",
+var factlist = ["I was Time's Person of The Year for 2006! Wow! :)",
 "I like gaming, I have over 30 different console emulators installed on my PC!",
 "While I do enjoy watching anime, I also like watching cartoons! It's not strictly for kids!",
 "I learnt cycling and swimming at the very young age... of 19. You're never too old to learn something new!",
-"I scored the lowest in my class for PSLE, but it has not stopped me from reaching my desired uni course!",
-"Along with programming, I am also skilled in graphic design, making data visuals, infographics and digital art",
-"I have 0 interest in anything related to National Service.",
+"I scored the lowest in my class for PSLE, pretty sure my class was filled with child prodigies.",
+"Along with programming, I am also skilled in graphic design. I made data visuals, infographics and digital art",
+"Unlike most guys in Singapore, I have 0 interest in anything related to National Service.",
 "I actually dislike reading novels & fictional books, I prefer non-fiction & informative magazines!",
-"One of my future ambitions is to start a web comic series about my life in Singapore!",
+"One of my future ambitions is to start my own web comic series!",
 "I have been to Japan, Taiwan, New Zealand & Australia, I hope to visit Europe & The Americas in the future."];
 
 function shuffleArray(array) {
@@ -51,7 +51,7 @@ $(".btn-success").pulsate({
     glow: true,
 });
 
-$(".btn-danger").pulsate({
+$(".btn-secondary").pulsate({
     color: $(".banner-heading").css("background-color"),
     glow: true,
 });
